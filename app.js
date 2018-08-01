028e3ecdf2aaec87ea452c919dd41f0ab26db244
jjmunt <41937167+jjmunt@users.noreply.github.com>
2018-08-01 13:26:03 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+// This is a JavaScript file
+
+
+   (function () {
+
+       function tratareEvenimente() {
+           var watchID = navigator.compass.watchHeading(onSuccess, onError, {frequency: 10});
+           document.querySelector("set").addEventListener("click", function() {
+              // Citesc unghiul
+              var u = document.querySelector("unghi").value;
+              //  Rotesc cadranul
+              var element = document.querySelector("cadran");
+              var rot = "rotate(" + (-u) + "deg)";
+              element.style.transform = rot;
+            });
+       }
+
+       ons.ready(function () {
+           console.log("Onsen UI is ready!");
+           tratareEvenimente();
+       });
+
+       function onSuccess(heading) {
+           var element = document.querySelector("indicator");
+           var rot = "rotate(" + (-heading.magneticHeading) + "deg)";
+           element.style.transform = rot;
+       }
+
+       function onError(compassError) {
+           ons.notification.alert('Compass error: ' + compassError.code);
+       }
+   })();
